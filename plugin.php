627124ead8d9f9ac781bb2707d696182878f506a
Ren <s0985514623@gmail.com>
2024-04-08 16:47:59 +0800

    feat:UpdateChecker

--- plugin.php
+++ plugin.php
@@ -17,11 +17,73 @@
 
 namespace J7\WpMyAppPlugin\MyApp\Inc;
 
-require_once __DIR__ . '/vendor/autoload.php';
-require_once __DIR__ . '/inc/admin.php';
+use YahnisElsts\PluginUpdateChecker\v5\PucFactory;
+if ( ! \class_exists( 'J7\WpMyAppPlugin\MyApp\Inc\Plugin' ) ) {
+	/**
+		 * Class Plugin
+		 */
+	final class Plugin {
+		const KEBAB       = 'r2-wc-product-addon';
+		const GITHUB_REPO = 'https://github.com/s0985514623/R2-WC-Product-Addon';
 
-// 整入舊得bundle代碼
-require_once __DIR__ . '/inc/custom/bundles/php/produce-bundles.php';
+		/**
+		 * Instance
+		 *
+		 * @var Plugin
+		 */
+		private static $instance;
 
-$instance = new Bootstrap();
-$instance->init();
+		/**
+		 * Constructor
+		 */
+		public function __construct() {
+			require_once __DIR__ . '/vendor/autoload.php';
+			require_once __DIR__ . '/inc/admin.php';
+
+			// 整入舊得bundle代碼
+			require_once __DIR__ . '/inc/custom/bundles/php/produce-bundles.php';
+
+			$this->plugin_update_checker();
+		}
+		/**
+		 * Plugin update checker
+		 *
+		 * @return void
+		 */
+		public function plugin_update_checker(): void {
+			$update_checker = PucFactory::buildUpdateChecker(
+				self::GITHUB_REPO,
+				__FILE__,
+				self::KEBAB
+			);
+			/**
+			 * Type
+			 *
+			 * @var \Puc_v4p4_Vcs_PluginUpdateChecker $update_checker
+			 */
+			$update_checker->setBranch( 'master' );
+			// if your repo is private, you need to set authentication
+			// $update_checker->setAuthentication(self::$github_pat);
+			$update_checker->getVcsApi()->enableReleaseAssets();
+		}
+
+		/**
+		 * Instance
+		 *
+		 * @return Plugin
+		 */
+		public static function instance() {
+			if ( empty( self::$instance ) ) {
+				self::$instance = new self();
+			}
+			return self::$instance;
+		}
+	}
+	// require_once __DIR__ . '/vendor/autoload.php';
+	// require_once __DIR__ . '/inc/admin.php';
+
+	// // 整入舊得bundle代碼
+	// require_once __DIR__ . '/inc/custom/bundles/php/produce-bundles.php';
+
+	Plugin::instance();
+}
